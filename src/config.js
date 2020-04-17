3eef0fcfc0ed9fcdbe2a677980d00b0e989e684a
Devree Lewis <devree.lewis@gmail.com>
2020-04-17 13:23:12 -0400

    running deploy

--- src/config.js
+++ src/config.js
@@ -3,6 +3,9 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://postgres@localhost/prlog",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://postgres@localhost/prlog_test",
   JWT_SECRET: process.env.JWT_SECRET || "secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "5m",
 };
