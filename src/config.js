9b4597d73d74a0970ddbe957a7cdb62e06462aa9
Devree Lewis <devree.lewis@gmail.com>
2020-04-21 15:40:03 -0400

    added ssl back

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
+  CLIENT_ORIGIN: "https://localhost:3000/",
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://postgres@localhost/prlog",
